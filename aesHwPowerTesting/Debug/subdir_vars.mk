7abd737500c99f2d898babbb0493ab41bf1ad3fe
jesseDMoore1994 <jaymen1994@hotmail.com>
2020-09-07 16:57:12 -0500

    Add project for power testing

---
+++ aesHwPowerTesting/Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr5969.cmd 
+
+C_SRCS += \
+../aes.c \
+../main.c 
+
+C_DEPS += \
+./aes.d \
+./main.d 
+
+OBJS += \
+./aes.obj \
+./main.obj 
+
+OBJS__QUOTED += \
+"aes.obj" \
+"main.obj" 
+
+C_DEPS__QUOTED += \
+"aes.d" \
+"main.d" 
+
+C_SRCS__QUOTED += \
+"../aes.c" \
+"../main.c" 
+
+
